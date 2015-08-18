e3fbc6aef1e21a28a2eda8037a7ea5224e99f234
songlijun <songlijun@vcinema.cn>
2015-08-18 15:41:44 +0800

    first commit

---
+++ shuxian_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shuxian_ios
+//
+//  Created by SongLijun on 15/8/18.
+//  Copyright © 2015年 itjh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
