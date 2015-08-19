a067e42670cde3606b2dee4948689c6362d92f8d
songlijun <songlijun@vcinema.cn>
2015-08-19 18:34:55 +0800

    添加图片放大

--- Podfile
+++ Podfile
@@ -1,5 +1,6 @@
 platform :ios,8.0
 pod 'SwiftColor'
+pod 'OTTableHeaderView'
 
 
 use_frameworks!
--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -52,8 +52,10 @@ code_sign_if_enabled() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework 'Pods/OTTableHeaderView.framework'
   install_framework 'Pods/SwiftColor.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework 'Pods/OTTableHeaderView.framework'
   install_framework 'Pods/SwiftColor.framework'
 fi
