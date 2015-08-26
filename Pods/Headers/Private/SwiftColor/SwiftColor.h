ab49a36b03de0050e14ff9e5a6fe902c0004b527
songlijun <songlijun@vcinema.cn>
2015-08-26 09:52:53 +0800

    remove dir pods

--- Pods/Headers/Private/SwiftColor/SwiftColor.h
+++
@@ -1,1 +0,0 @@
-../../../SwiftColor/SwiftColor/SwiftColor.h
--- Pods/Target Support Files/SwiftColor/SwiftColor-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "SwiftColor.h"
-
-FOUNDATION_EXPORT double SwiftColorVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftColorVersionString[];
-
