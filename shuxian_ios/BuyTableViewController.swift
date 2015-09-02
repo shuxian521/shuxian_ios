9c8e12035379e3083a4f080aaa672541fa32c15f
itjhdev <github@itjh.com.cn>
2015-09-02 15:10:07 +0800

    删除购物车商品

--- shuxian_ios/BuyTableViewController.swift
+++ shuxian_ios/BuyTableViewController.swift
@@ -24,7 +24,7 @@ class BuyTableViewController: UITableViewController {
         if let barFont = UIFont(name: "AvenirNextCondensed-DemiBold", size: 18.0) {
             self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName:Color(hexString: "#228B22"), NSFontAttributeName:barFont]
         }
- 
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -126,8 +126,6 @@ class BuyTableViewController: UITableViewController {
         
         self.navigationController?.presentPopinController(popin, animated: true, completion: { () -> Void in
             
-            
-            
             print("弹窗")
         })
         
@@ -155,25 +153,29 @@ class BuyTableViewController: UITableViewController {
         
     }
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+   
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            //tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            
+//            [self.list removeObjectAtIndex:row];
+//            [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+//                withRowAnimation:UITableViewRowAnimationAutomatic];
+            
+            
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
